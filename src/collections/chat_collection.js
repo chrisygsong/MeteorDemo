ade0fd0c603b1bf1dd96307270ae1f8e35810641
chrisygsong <chris.ygsong@gmail.com>
2015-06-25 15:54:41 +0900

    changed folder

--- src/collections/chat_collection.js
+++
@@ -1,7 +0,0 @@
-/**
- * author : SungYong Jang <jsy@adain.kr>
- * date : 2015-06-22
- * description : chat collection
- */
-
-ChatCollection = new Mongo.Collection('Chat');
--- src/server/chat_publication.js
+++
@@ -1,9 +0,0 @@
-/**
- * author : SungYong Jang <jsy@adain.kr>
- * date : 2015-06-22
- * description : chat publish
- */
-
-Meteor.publish('chat', function () {
-  return ChatCollection.find({});
-});
